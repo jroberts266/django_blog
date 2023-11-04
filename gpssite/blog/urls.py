1ea9d7c29027c53ea4f23ec8b5886a729ac54fb4
jroberts266 <jroberts266@ivytech.edu>
2023-11-03 20:01:45 -0400

    added view and template for detailed blog post as well as CSS for the site

--- gpssite/blog/urls.py
+++ gpssite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
